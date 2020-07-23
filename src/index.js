038a4cc025c88f9559f871386d2f639acd4f5c92
brithe00 <bthellier51@gmail.com>
2020-07-23 12:19:44 +0200

    refactor(indexjs): added reactstrap and did update to add redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { createStore, combineReducers } from 'redux';
+import Reducer from './redux/Reducer';
+import { Provider } from 'react-redux';
+
+const rootReducer = combineReducers({
+  reducer: Reducer,
+})
+
+const store = createStore(rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
