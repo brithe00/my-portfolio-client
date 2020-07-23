18c043734e9f6db1ae1bb31b1b125d0858491f41
brithe00 <bthellier51@gmail.com>
2020-07-23 12:29:11 +0200

    build(navbarjsx): built a navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import './Navbar.css'
+
+import { useHistory } from 'react-router-dom';
+
+function Navbar(props) {
+
+    const history = useHistory();
+
+    return (
+        <>
+            <div className="navbar-container">
+                <div className="logo-container">
+                    <div className="title-container">My Portfolio</div>
+                    <div className="subtitle-container">Admin panel</div>
+                </div>
+                <div className="buttons-container">
+                    <Button color="secondary" size="lg" onClick={() => {
+                        history.push('/');
+                    }}>Redirect to website</Button>
+                    
+                    <Button color="primary" size="lg" onClick={() => {
+                        history.push('/admin');
+                    }}>Redirect to login</Button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
